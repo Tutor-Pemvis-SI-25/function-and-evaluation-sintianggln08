c9416205b91d8135643295d1eda2d6b1b815d0b7
sintianggln08 <sintianeng0812@gmail.com>
2025-11-18 21:12:53 +0700

    TLAST

--- TLAST.java
+++ TLAST.java
@@ -1,2 +1,44 @@
-//Nama - NIM
+//sintia Geni Audi Nainggolan - 12S25035
 
+import java.util.*;
+import java.lang.Math;
+
+public class TLAST {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int totalData;
+        String command;
+        int[] deskripsi = new int[10], kode = new int[10], matkul = new int[10], dosen = new int[10], deadline = new int[10], ketID = new int[10], status = new int[10];
+        int[] kesulitan = new int[10], hari = new int[10];
+        double[] prioritas = new double[10];
+
+        totalData = 0;
+        while (!command.equals("---")) {
+            command = input.nextLine();
+            if (command.equals("AddTask")) {
+                addTask();
+            } else {
+                if (UpdateTask) {
+                    UpdateTask();
+                } else {
+                    if (ShowAssignment) {
+                        ShowAssignment();
+                    } else {
+                        if (command.equals("---")) {
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public static void addTask() {
+        if (totalData < 10) {
+            Deskripsi[totalData] = input.nextLine();
+        } else {
+            if (Deskripsi[totalData] == "---") {
+            }
+        }
+    }
+}
